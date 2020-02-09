9e5ee0cdc24ee77c6d42ec49af59789343b8932a
EsraaMamoun <esraamamoun5@gmail.com>
2020-02-09 15:40:16 +0200

    .

--- js/caontactus.js
+++
@@ -1,9 +0,0 @@
-var pass=prompt ("Did you pass: yes or no");
-if(pass=="yes"){alert("goog job");}
-else{alert("oh nooooo!");}
-
-var pass_mark=prompt("What is the passing grade?");
-while(pass_mark != 50){
-    pass_mark=prompt("no try again");
-};
-alert("yes its 50");
--- js/scrept-phy.js
+++
@@ -1,9 +0,0 @@
-console.log("I Like JS");
-
-var username;
-var message;
-username = 'Hello';
-message = 'WELCOME TO PHYSICS LECTURE WEB';
-
-console.log(username);
-
